a25d4c168d7968fd274a1ad8559e01f4f6e9ca53
Aleksey B <boev.aleksey@gmail.com>
2015-12-01 00:54:13 +1300

    Implemented new

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,10 +1,31 @@
+require 'constants'
+
 class GameController < ApplicationController
 skip_before_filter :verify_authenticity_token
 
 def new
-  json_body = JSON.parse(request.body.read)
-  $redis.publish 'events', json_body.to_json
-  msg = { :result => "OK" }
+  game_id = 0
+  games = Game.all
+  games.each do |game|
+    game_details = JSON.parse(game.details)
+    if ((game_details[Constants::JSON_GAME_STATUS] == Game::STATUS_SEARCHING_PLAYERS) and @socket_id != nil )
+      game_details[Constants::JSON_GAME_PLAYERS][@socket_id] = Game::PLAYER_STATUS_WAITING
+      game_details[Constants::JSON_GAME_STATUS] = Game::STATUS_IN_PROGRESS
+      game.details = game_details.to_json
+      game.save
+      game.start_game
+      game_id = game.id
+      break
+    end
+  end
+  if ((game_id == 0) and (@socket_id != nil) )
+    game = Game.new
+    game_details = { Constants::JSON_GAME_PLAYERS => {@socket_id => Game::PLAYER_STATUS_WAITING}, Constants::JSON_GAME_STATUS  => Game::STATUS_SEARCHING_PLAYERS}
+    game.details = game_details.to_json
+    game.save
+    game_id = game.id
+  end
+  msg = { :result => Constants::RESULT_OK, :data => game_details }
   respond_to do |format|
     format.json  { render :json => msg }
   end
