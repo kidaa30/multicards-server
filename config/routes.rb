9f6a68c07627690625e4984a13ab5c73725aad58
Aleksey B <boev.aleksey@gmail.com>
2015-12-01 00:55:45 +1300

    Added post user and post event paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  post 'user' => 'user#new'
   post 'game' => 'game#new'
+  post 'event' => 'event#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
