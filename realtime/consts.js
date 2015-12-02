1cfde3d953647bca0b9fdbbcb14325c13d64adac
Aleksey B <boev.aleksey@gmail.com>
2015-12-02 19:59:47 +1300

    Added consts module

---
+++ realtime/consts.js
@@ -0,0 +1,8 @@
+exports.SOCK_CHANNEL = "events";
+exports.RAILS_SERVER = "http://localhost:80/event";
+
+exports.JSON_SOCK_MSG_TYPE = "type";
+exports.JSON_SOCK_MSG_BODY = "body";
+exports.SOCK_MSG_TYPE_ANNOUNCE_SOCKETID = "socket_id_announce";
+exports.SOCK_MSG_TYPE_PLAYER_STATUS_UPDATE = "player_status";
+exports.SOCK_MSG_TYPE_SOCKET_CLOSE    ="socket_disconnected"
