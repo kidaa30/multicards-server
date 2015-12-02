cd2629518efc85c8a7931858087d3f535ff569f6
Aleksey B <boev.aleksey@gmail.com>
2015-12-02 20:00:41 +1300

    Moved all socket logic from realtime -> protocol module

--- realtime/realtime.js
+++ realtime/realtime.js
@@ -1,71 +1,31 @@
-var app = require('http').createServer(handler);
+var app = require('http').createServer();
 var server = app.listen(5001);
 var io = require('socket.io').listen(server);
 var fs = require('fs');
 var redis = require('redis').createClient();
-var consts = require('./const.js');
+var consts = require('./consts.js');
 var http = require('http');
 var utf8 = require('utf8');
+var protocol = require('./protocol.js');
 http.post = require('http-post');
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
 
 redis.subscribe(consts.SOCK_CHANNEL);
 
 redis.on('message', function(channel, message){
-  console.log('got redis message ' + message);
-  msg = JSON.parse(message);
-  msg["test"] =  utf8.encode("искренний");
-  for (var i = 0; i < msg.id_to.length; i++) {
-    io.to(msg.id_to[i]).emit('event', message);
-    console.log('sending message to ' + msg.id_to[i]);
-  }
+  protocol.msg_int(io, message);
 });
 
 io.on('connection', function(socket){
-  console.log(socket.id + ' connected');
-
-  msg = JSON.stringify( { msg_type: consts.SOCK_MSG_TYPE_ANNOUNCE_SOCKETID, msg_body: socket.id } );
-  io.to(socket.id).emit('event', msg);
+  protocol.announce_socket_id(io, socket.id);
 
   socket.on('message', function(message){
-    console.log('got message from client: ' + message);
-    msg = JSON.parse(message);
-    if (msg.id_to != null) {
-      for (var i = 0; i < msg.id_to.length; i++) {
-        io.to(msg.id_to[i]).emit('event', message);
-      }
-    } else {
-      http.post(consts.RAILS_SERVER, msg, function(res){
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {
-          console.log(chunk);
-        });
-      });      
-    }
+    protocol.msg_ext(io, socket, message);
   });
 
   socket.on('disconnect', function() {
     console.log('Disconnected ' + socket.id);
-    //msg = JSON.stringify( {msg_type: consts.SOCK_MSG_TYPE_SOCKET_CLOSE, msg_body: socket.id } );
-    msg = {msg_type: consts.SOCK_MSG_TYPE_SOCKET_CLOSE, msg_body: socket.id };
-    http.post(consts.RAILS_SERVER, msg, function(res){
-      res.setEncoding('utf8');
-      res.on('data', function(chunk) {
-        console.log(chunk);
-      });
-    });
+    json_msg = protocol.make_json_msg(null, consts.SOCK_MSG_TYPE_SOCKET_CLOSE, socket.id);
+    protocol.http_post_local(json_msg);
   });
 
 });
