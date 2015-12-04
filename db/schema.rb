9504086243a81c0ded341ac7f148fae49113a9a1
Aleksey B <boev.aleksey@gmail.com>
2015-12-04 20:15:59 +1300

    Added cardsets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128080058) do
+ActiveRecord::Schema.define(version: 20151203102203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,12 +21,21 @@ ActiveRecord::Schema.define(version: 20151128080058) do
     t.string   "back"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "set_id"
+  end
+
+  create_table "cardsets", force: true do |t|
+    t.string   "language"
+    t.string   "details"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "games", force: true do |t|
     t.string   "details"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "status"
   end
 
   create_table "users", force: true do |t|
