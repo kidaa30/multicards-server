9593f73c726fc9284e92e3a7dbc059cbaf3e6e7b
Aleksey B <boev.aleksey@gmail.com>
2015-12-15 03:25:21 +1300

    Refactor new game logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,25 +4,19 @@ class GameController < ApplicationController
 skip_before_filter :verify_authenticity_token
 
 def new
-  game_id = nil
-  games = Game.all
   game_details = {}
-  games.each do |game|
+  game = Game.where(status: Game::STATUS_SEARCHING_PLAYERS).first
+  if (game != nil)
     game_details = JSON.parse(game.details)
-    if ((game_details[Constants::JSON_GAME_STATUS] == Game::STATUS_SEARCHING_PLAYERS) and @socket_id != nil )
-      game_details[Constants::JSON_GAME_PROFILES][@socket_id] = @user.get_details
-      game_details[Constants::JSON_GAME_PLAYERS][@socket_id] = Game::PLAYER_STATUS_WAITING
-      game_details[Constants::JSON_GAME_STATUS] = Game::STATUS_IN_PROGRESS
-      game_details[Constants::JSON_GAME_QUESTIONCNT] = 0
-      game.status = Game::STATUS_IN_PROGRESS
-      game.details = game_details.to_json
-      game.save
-      game.start_game
-      game_id = game.id
-      break
-    end
-  end
-  if ((game_id == nil) and (@socket_id != nil))
+    game_details[Constants::JSON_GAME_PROFILES][@socket_id] = @user.get_details
+    game_details[Constants::JSON_GAME_PLAYERS][@socket_id] = Game::PLAYER_STATUS_WAITING
+    game_details[Constants::JSON_GAME_STATUS] = Game::STATUS_IN_PROGRESS
+    game_details[Constants::JSON_GAME_QUESTIONCNT] = 0
+    game.status = Game::STATUS_IN_PROGRESS
+    game.details = game_details.to_json
+    game.save
+    game.start_game
+  else
     game = Game.new
     game_details[Constants::JSON_GAME_PLAYERS] = {@socket_id => Game::PLAYER_STATUS_WAITING}
     game_details[Constants::JSON_GAME_STATUS] = Game::STATUS_SEARCHING_PLAYERS
@@ -31,7 +25,6 @@ def new
     game.status = Game::STATUS_SEARCHING_PLAYERS
     game.details = game_details.to_json
     game.save
-    game_id = game.id
   end
   msg = { :result => Constants::RESULT_OK, :data => game_details }
   respond_to do |format|
