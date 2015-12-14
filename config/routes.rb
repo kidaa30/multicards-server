3306fc91f667678f215b845891f923b103d5c83d
Aleksey B <boev.aleksey@gmail.com>
2015-12-15 02:05:19 +1300

    Added put user and post image paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   post 'user' => 'user#new'
+  put 'user' => 'user#update'
   post 'game' => 'game#new'
   post 'event' => 'event#new'
+  post 'image' => 'upload#upload'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
