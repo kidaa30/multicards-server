6c18f0abf92160f4b21205f753da27f3f7af22f7
Aleksey B <boev.aleksey@gmail.com>
2015-12-16 23:44:18 +1300

    Updated log comment

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -6,9 +6,9 @@ skip_before_filter :verify_authenticity_token
 skip_before_filter :check_credentials
 
 def new
+  #Rails::logger.debug request.body.read
   json_body = JSON.parse(request.body.read)
   res = Protocol.parse_msg(json_body)
-
   respond_to do |format|
     format.json  { render :json => res }
   end
