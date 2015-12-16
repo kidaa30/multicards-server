d38d0869f86a492a77db10649489197dac6c90f6
Aleksey B <boev.aleksey@gmail.com>
2015-12-17 08:11:27 +1300

    Updated constants

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,37 +1,24 @@
 class Constants
-  RESULT_OK 		= 	"OK"
-  RESULT_ERROR		= 	"ERROR"
-
-  ERROR_JSON_PARSE 	= 	101
-  MSG_JSON_PARSE 	= 	"JSON parse error"
-  ERROR_BODY_FORMAT 	= 	102
-  MSG_BODY_FORMAT 	= 	"Missing required argument in body"
-  ERROR_NOT_FOUND	=	103
-  MSG_NOT_FOUND		=	"Entry not found"
-
-  HEADER_USERID		=	"id"
-  HEADER_SOCKETID	=	"socketid"
-
-  SOCK_CHANNEL		=	"events"
-
-  SOCK_EV_NEW_GAME	=	"game_start"
-
   SOCK_MSG_TYPE_NEW_EVENT=	"new_event"
   SOCK_MSG_TYPE_NEW_QUESTION=	"new_question"
   SOCK_MSG_TYPE_PLAYER_STATUS_UPDATE="player_status"
   SOCK_MSG_TYPE_PLAYER_ANSWERED	="player_answered"
+  SOCK_MSG_TYPE_PLAYER_TYPED	="player_typed"
   SOCK_MSG_TYPE_SOCKET_CLOSE	="socket_disconnected"
   SOCK_MSG_TYPE_GAME_END	="game_end"
   SOCK_MSG_TYPE_QUIT_GAME	="quit_game"
   SOCK_MSG_TYPE_GAME_START	="game_start"
+  SOCK_MSG_TYPE_ANNOUNCE_USERID	="announce_userid"
 
   JSON_GAME_ID		=	"game_id"
   JSON_GAME_STATUS	=	"status"
   JSON_GAME_PLAYERS	=	"players"
+  JSON_GAME_SCORES	=	"scores"
   JSON_GAME_PROFILES	=	"profiles"
   JSON_GAME_CURQUESTION	=	"question"
   JSON_GAME_QUESTIONCNT	=	"question_count"
   JSON_GAME_QUESTIONTYPE=	"question_type"
+  JSON_GAME_SETID	=	"setid"
 
   JSON_USER_SOCKETID	=	"socketid"
   JSON_USER_ID		=	"id"
@@ -52,6 +39,22 @@ class Constants
   JSON_QST_OPTIONS	=	"options"
   JSON_QST_ANSWER_ID	=	"answer_id"
 
+  RESULT_OK             =       "OK"
+  RESULT_ERROR          =       "ERROR"
+
+  ERROR_JSON_PARSE      =       101
+  MSG_JSON_PARSE        =       "JSON parse error"
+  ERROR_BODY_FORMAT     =       102
+  MSG_BODY_FORMAT       =       "Missing required argument in body"
+  ERROR_NOT_FOUND       =       103
+  MSG_NOT_FOUND         =       "Entry not found"
+
+  HEADER_USERID         =       "id"
+  HEADER_SOCKETID       =       "socketid"
+
+  SOCK_CHANNEL          =       "events"
+
+  SOCK_EV_NEW_GAME      =       "game_start"
 
 
 end
